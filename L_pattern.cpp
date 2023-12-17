03d843d58879a95f397d51312a8ddd2d108d509d
Yash Lade <150010042+Yash-Lade@users.noreply.github.com>
2023-12-17 17:07:24 +0530

    Program to print L pattern

---
+++ L_pattern.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int i,j;
+	char c=178;
+	
+	for(int i=0;i<=6;i++)
+	{
+		for(j=0;j<=7;j++)
+		{
+			if(((i%7!=0) && (j==0)) || ((i==6)&&(j%8!=0)))
+			cout<<c;
+			
+			else
+			cout<<" ";
+		}
+		cout<<endl;
+	}
+}
