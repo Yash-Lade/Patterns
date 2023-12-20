a5cfaa7739ea4dc67e5de120a45e1b9d0770dba8
Yash Lade <150010042+Yash-Lade@users.noreply.github.com>
2023-12-20 10:45:29 +0530

    Program to print N
    This is the code to print N using for loop in C++

---
+++ N_pattern.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int i,j;
+	
+	cout<<"The pattern is as follows :\n\n";
+	
+	for(i=0;i<=7;i++)
+	{
+		for(j=0;j<=8;j++)
+		{
+			if((i%8!=0 && j==0) || (i%8!=0 && j==8) || (i-j==0) || (i==0 && j==8))
+			cout<<"*";
+			
+			else
+			cout<<" ";
+		}
+		cout<<endl;
+	}
+}
