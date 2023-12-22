b1792146d7b0bd536817286fa475053492cf6dd5
Yash Lade <150010042+Yash-Lade@users.noreply.github.com>
2023-12-22 19:38:42 +0530

    Program to print I pattern
    This is the C++ code to print the alphabet I.

---
+++ I_pattern.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int i,j;
+	char ch=178;
+	
+	cout<<"The pattern is as follows :\n\n";
+	
+	for(i=0;i<=7;i++)
+	{
+		for(j=0;j<=8;j++)
+		{
+			if((i%8!=-1 && j==4) || (i==0 || i==7 ) && j%9!=-1)
+			cout<<ch;
+			
+			else
+			cout<<" ";
+		}
+		cout<<endl;
+	}
+}
