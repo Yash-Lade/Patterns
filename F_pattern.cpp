c480f954e7f92e4924dd5da692c4e0200e79a7eb
Yash Lade <150010042+Yash-Lade@users.noreply.github.com>
2023-12-22 19:36:06 +0530

    Program to print F pattern
    This is the code to print the alphabet F.

---
+++ F_pattern.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int i,j;
+	
+	cout<<"The pattern is as follows :\n\n";
+	
+	for(i=0;i<=7;i++)
+	{
+		for(j=0;j<=8;j++)
+		{
+			if((i%8!=-1 && j==0) || (i==0 || i==3 ) && j%9!=0)
+			cout<<"*";
+			
+			else
+			cout<<" ";
+		}
+		cout<<endl;
+	}
+}
