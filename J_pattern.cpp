9b593bbe08f8c249c60b14eee8841b9222738725
Yash Lade <150010042+Yash-Lade@users.noreply.github.com>
2023-12-23 19:07:49 +0530

    Program to print J pattern
    This is the code to print the alphabet J

---
+++ J_pattern.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int i,j;
+	
+	cout<<"The patternm is as follows ::\n";
+	for(i=1;i<=9;i++)
+	{
+		for(j=1;j<=9;j++)
+		{
+			if((i==1 && j%10!=0) || (i%10!=0 && j==5) || ((i==9 && j%6!=0) && j<6) || (i%5!=0 && j==1) && i>4)
+			cout<<"*";
+			
+			else
+			cout<<" ";
+		}
+		cout<<endl;
+	}
+}
