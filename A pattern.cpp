d79fdbab8092f89e2ac5d0f98429775bb84fac0b
Yash Lade <150010042+Yash-Lade@users.noreply.github.com>
2024-01-02 17:34:23 +0530

    Program to print A pattern
    This is the code to print alphabet A

---
+++ A pattern.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int i,j;
+	
+	cout<<"The pattern is as follows :\n";
+	
+	for(i=0;i<=6;i++)
+	{
+		for(j=0;j<=10;j++)
+		{
+			if((i+j==5) || ((j-i==5) && i>=1 && j>=6) || (i==3 && j>=2 && j<=8))
+			cout<<"*";
+			
+			else
+			cout<<" ";
+		}
+		cout<<endl;
+	}
+}
