fcac8fb3797ae43cfb06e005dbabfb6d6d47bb4f
Yash Lade <150010042+Yash-Lade@users.noreply.github.com>
2024-01-03 08:09:08 +0530

    Code to print B pattern
    This is the program to print the pattern of alphabet B

---
+++ B pattern.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int i,j;
+	char c=219;
+	
+	cout<<"The pattern is as follows :\n\n";
+	
+	for(i=0;i<=6;i++)
+	{
+		for(j=0;j<=7;j++)
+		{
+			if(((i==0 || i==3 || i==6) && j%8!=7) || (i%6!=7 && j==0) || (i%3!=0 && j==7))
+			cout<<c;
+			
+			else
+			cout<<" ";
+
+		}
+		cout<<endl;
+	}
+}
